f29675fb012147ca45368cef0c0ab7b614516638
mccxiv <adr.stella@gmail.com>
2016-07-28 23:28:21 -0600

    Use temporary domain

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
   __WEBPACK_CLIENT_ID__: '"7o81g9xzrjdksjtm1hlgdix9sbq8thb"',
-  __REDIRECT_URI__: '"http://tcm.gettc.xyz/"'
+  __REDIRECT_URI__: '"https://mccxiv.github.io/twitch-chat-go/"'
 }
