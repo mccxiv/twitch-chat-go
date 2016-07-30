826d95e3345e141fcac62bdb50548f658315034c
mccxiv <adr.stella@gmail.com>
2016-07-30 07:00:04 -0600

    Add favicon

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import 'font-awesome/css/font-awesome.css'
 import Vue from 'vue'
 import Resource from 'vue-resource'
 import App from './components/app'
+import setFavicon from './lib/set-favicon'
 
+setFavicon()
 Vue.use(Resource)
 new Vue({el: 'body', components: {App}})
