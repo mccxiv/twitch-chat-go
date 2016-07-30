ebd74dab1636e0d9238efd2897b348f83ad05477
mccxiv <adr.stella@gmail.com>
2016-07-30 07:04:33 -0600

    Fix redirect uri

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
   __WEBPACK_CLIENT_ID__: '"7o81g9xzrjdksjtm1hlgdix9sbq8thb"',
-  __REDIRECT_URI__: '"http://go.gettc.xyz/"'
+  __REDIRECT_URI__: '"http://go.gettc.xyz"'
 }
