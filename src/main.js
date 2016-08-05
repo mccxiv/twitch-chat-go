d52251e707c4249a7c51ec049c45dc8870f6a2e5
mccxiv <adr.stella@gmail.com>
2016-08-05 12:55:20 -0600

    Use static favicon, no need for it to be a module

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import 'font-awesome/css/font-awesome.css'
 import Vue from 'vue'
 import Resource from 'vue-resource'
 import App from './components/app'
-import setFavicon from './lib/set-favicon'
 
-setFavicon()
 Vue.use(Resource)
 new Vue({el: 'body', components: {App}})
