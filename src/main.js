5d9f0176c648213f21258070e2437d9f69703b35
mccxiv <adr.stella@gmail.com>
2016-08-06 11:15:32 -0600

    Try to catch all errors

--- src/main.js
+++ src/main.js
@@ -3,5 +3,7 @@ import Vue from 'vue'
 import Resource from 'vue-resource'
 import App from './components/app'
 
+window.onerror = message => window.alert(message)
+
 Vue.use(Resource)
 new Vue({el: 'body', components: {App}})
